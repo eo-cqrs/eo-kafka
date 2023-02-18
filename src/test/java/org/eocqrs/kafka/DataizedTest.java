ffb3adaf250c915d387f2d26586632d418466e28
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-02-18 17:26:17 +0300

    for coverage deployment only

---
+++ src/test/java/org/eocqrs/kafka/DataizedTest.java
@@ -0,0 +1,15 @@
+package org.eocqrs.kafka;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Aliaksei Bialiauski (abialiauski@solvd.com)
+ * @since 1.0
+ */
+class DataizedTest {
+
+  @Test
+  void coverage() {
+
+  }
+}
