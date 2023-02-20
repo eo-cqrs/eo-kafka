b3d5686e053dd18f9e8a9a823a8a13076ead4782
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-02-20 14:02:19 +0300

    poc

--- src/main/java/org/eocqrs/kafka/Producer.java
+++ src/main/java/org/eocqrs/kafka/Producer.java
@@ -1,5 +1,6 @@
 package org.eocqrs.kafka;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
@@ -11,9 +12,10 @@ public interface Producer<K, X> {
 
   void send(K key, Data<X> message) throws Exception;
 
+  @RequiredArgsConstructor
   final class S<T> implements Producer<String, T> {
 
-    KafkaProducer<String, T> origin;
+    private final KafkaProducer<String, T> origin;
 
     @Override
     public void send(final String key, final Data<T> data) throws Exception {
@@ -22,10 +24,6 @@ public interface Producer<K, X> {
           data.topic(),
           data.partition(),
           key,
-/**
- * @todo #1:30m/DEV KfData implementation
- *
- */
           new DataizeOf<>(data.dataized(), "text")
             .dataize()
         )
