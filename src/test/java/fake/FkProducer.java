0470df4c0fa91ce231abd7b0a8afb80439c6af67
Ivan <clicker.heroes.acg@gmail.com>
2023-02-25 05:55:27 +0300

    fake to test root

--- src/test/java/fake/FkProducer.java
+++ src/test/java/fake/FkProducer.java
@@ -20,11 +20,23 @@
  * SOFTWARE.
  */
 
+package fake;
+
+import org.eocqrs.kafka.Data;
+import org.eocqrs.kafka.Producer;
+
 /**
- * Fake.
- *
  * @author Aliaksei Bialiauski (abialiauski.dev@gmail.com)
  * @since 0.0.0
  */
 
-package org.eocqrs.kafka.fake;
+/**
+ * @todo #10:45m/DEV Fake Producer implementation
+ */
+public class FkProducer<K, X> implements Producer<K, X> {
+
+  @Override
+  public void send(final K key, final Data<X> message) {
+    throw new UnsupportedOperationException("#send()");
+  }
+}
--- src/test/java/fake/package-info.java
+++ src/test/java/fake/package-info.java
@@ -20,23 +20,11 @@
  * SOFTWARE.
  */
 
-package org.eocqrs.kafka.fake;
-
-import org.eocqrs.kafka.Data;
-import org.eocqrs.kafka.Producer;
-
 /**
+ * Fake.
+ *
  * @author Aliaksei Bialiauski (abialiauski.dev@gmail.com)
  * @since 0.0.0
  */
 
-/**
- * @todo #10:45m/DEV Fake Producer implementation
- */
-public class FkProducer<K, X> implements Producer<K, X> {
-
-  @Override
-  public void send(final K key, final Data<X> message) {
-    throw new UnsupportedOperationException("#send()");
-  }
-}
+package fake;
