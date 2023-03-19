339565db2ef324689a1cb4a91873ecf0d7ef4802
Ivan <clicker.heroes.acg@gmail.com>
2023-03-19 20:11:24 +0300

    javadocs

--- src/main/java/io/github/eocqrs/kafka/consumer/settings/KfConsumerParams.java
+++ src/main/java/io/github/eocqrs/kafka/consumer/settings/KfConsumerParams.java
@@ -7,9 +7,19 @@ import org.cactoos.text.FormattedText;
 
 import java.util.Collection;
 
+/**
+ * Kafka params for {@link KfConsumerSettings}.
+ * Decorates {@link Settings}.
+ *
+ * @author Ivan Ivanchuk (l3r8y@duck.com)
+ * @since 0.0.2
+ */
 @RequiredArgsConstructor
 public final class KfConsumerParams implements Settings {
 
+  /**
+   * The origin.
+   */
   private final Settings origin;
 
   @Override
@@ -20,7 +30,7 @@ public final class KfConsumerParams implements Settings {
   @Override
   public String asXml() {
     return new FormattedText(
-      "<consumer>\n%s\n</consumer>",
+      "<consumer>\n%s</consumer>\n",
       this.origin.asXml()
     ).toString();
   }
--- src/main/java/io/github/eocqrs/kafka/producer/settings/KfProducerParams.java
+++ src/main/java/io/github/eocqrs/kafka/producer/settings/KfProducerParams.java
@@ -1,3 +1,25 @@
+/*
+ *  Copyright (c) 2022 Aliaksei Bialiauski, EO-CQRS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package io.github.eocqrs.kafka.producer.settings;
 
 import io.github.eocqrs.kafka.Settings;
@@ -7,9 +29,20 @@ import org.cactoos.text.FormattedText;
 
 import java.util.Collection;
 
+/**
+ * Kafka Producer parameters.
+ * Decorates {@link Settings}.
+ *
+ * @author Aliaksei Bialiauski (abialiauski.dev@gmail.com)
+ * @author Ivan Ivanchuk (l3r8y@duck.com)
+ * @since 0.0.0
+ */
 @RequiredArgsConstructor
 public final class KfProducerParams implements Settings {
 
+  /**
+   * The origin.
+   */
   private final Settings origin;
 
   @Override
@@ -20,7 +53,7 @@ public final class KfProducerParams implements Settings {
   @Override
   public String asXml() {
     return new FormattedText(
-      "<producer>\n%s\n</producer>",
+      "<producer>\n%s</producer>\n",
       this.origin.asXml()
     ).toString();
   }
