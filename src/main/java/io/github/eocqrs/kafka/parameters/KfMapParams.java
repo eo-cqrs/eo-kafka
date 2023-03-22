a01765a82800da15d3e97cdf11749e6444a8a643
Ivan <clicker.heroes.acg@gmail.com>
2023-03-22 03:45:37 +0300

    map params as separate class

---
+++ src/main/java/io/github/eocqrs/kafka/parameters/KfMapParams.java
@@ -0,0 +1,58 @@
+package io.github.eocqrs.kafka.parameters;
+
+import com.jcabi.xml.XML;
+import com.jcabi.xml.XMLDocument;
+import io.github.eocqrs.kafka.xml.TextXpath;
+import lombok.RequiredArgsConstructor;
+import org.cactoos.Scalar;
+import org.cactoos.text.Concatenated;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * It takes an XML document and a customer name, and returns a map of the customer's data.
+ *
+ * @author Ivan Ivanchuk (l3r8y@duck.com)
+ * @since 0.0.2
+ */
+@RequiredArgsConstructor
+public final class KfMapParams implements Scalar<Map<String, Object>> {
+
+  /**
+   * It's a regex that matches all capital letters, except the first one.
+   */
+  private static final Pattern CAPITALS = Pattern.compile("(?<!^)([A-Z])");
+
+  /**
+   * The origin config.
+   */
+  private final XML configuration;
+
+  /**
+   * Consumer or Producer.
+   */
+  private final KfCustomer customer;
+
+  @Override
+  public Map<String, Object> value() throws Exception {
+    final String parent = this.customer.toString().toLowerCase(Locale.ROOT);
+    return new XMLDocument(this.configuration.toString())
+      .nodes(new Concatenated("//", parent, "/*").toString())
+      .stream()
+      .map(Object::toString)
+      .map(XMLDocument::new)
+      .map(xml -> xml.nodes("//*").get(0).node().getNodeName())
+      .collect(
+        Collectors.toMap(
+          name -> KfMapParams.CAPITALS
+            .matcher(name)
+            .replaceAll(".$1")
+            .toLowerCase(Locale.ROOT),
+          name -> new TextXpath(this.configuration, "//".concat(name)).toString()
+        )
+      );
+  }
+}
--- src/main/java/io/github/eocqrs/kafka/parameters/KfXmlFlexible.java
+++ src/main/java/io/github/eocqrs/kafka/parameters/KfXmlFlexible.java
@@ -1,17 +1,10 @@
 package io.github.eocqrs.kafka.parameters;
 
-import com.jcabi.xml.XMLDocument;
 import io.github.eocqrs.kafka.ConsumerSettings;
 import io.github.eocqrs.kafka.ProducerSettings;
-import io.github.eocqrs.kafka.xml.TextXpath;
+import lombok.SneakyThrows;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.cactoos.text.Concatenated;
-
-import java.util.Locale;
-import java.util.Map;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 /**
  * @todo #154:30m/DEV Get rid of deprecated api.
@@ -27,52 +20,29 @@ public final class KfXmlFlexible<K, X> extends KfFlexibleEnvelope<K, X>
   implements ConsumerSettings<K, X>, ProducerSettings<K, X> {
 
   /**
-   * It's a regex that matches all capital letters, except the first one.
-   */
-  private static final Pattern CAPITALS = Pattern.compile("(?<!^)([A-Z])");
-
-  /**
    * Ctor.
    *
    * @param name Name of XML configuration placed in resources folder.
-   * @throws Exception When something went wrong.
    */
-  public KfXmlFlexible(final String name) throws Exception {
+  @SneakyThrows
+  public KfXmlFlexible(final String name) {
     super(name);
   }
 
   @Override
+  @SneakyThrows
   public KafkaConsumer<K, X> consumer() {
-    return new KafkaConsumer<K, X>(this.xmlConfigAsMap("consumer"));
+    return new KafkaConsumer<K, X>(
+      new KfMapParams(this.settings, KfCustomer.CONSUMER).value()
+    );
   }
 
   @Override
+  @SneakyThrows
   public KafkaProducer<K, X> producer() {
-    return new KafkaProducer<K, X>(this.xmlConfigAsMap("producer"));
+    return new KafkaProducer<K, X>(
+      new KfMapParams(this.settings, KfCustomer.PRODUCER).value()
+    );
   }
 
-  /**
-   * It takes a string, uses it to find a bunch of nodes in an XML document,
-   * then returns a map of the kafka-names to their values.
-   *
-   * @param type The type of the configuration to be loaded.
-   * @return A map of the XML configuration file.
-   */
-  private Map<String, Object> xmlConfigAsMap(final String type) {
-    return new XMLDocument(this.settings.toString())
-      .nodes(new Concatenated("//", type, "/*").toString())
-      .stream()
-      .map(Object::toString)
-      .map(XMLDocument::new)
-      .map(xml -> xml.nodes("//*").get(0).node().getNodeName())
-      .collect(
-        Collectors.toMap(
-          name -> KfXmlFlexible.CAPITALS
-            .matcher(name)
-            .replaceAll(".$1")
-            .toLowerCase(Locale.ROOT),
-          name -> new TextXpath(this.settings, "//".concat(name)).toString()
-        )
-      );
-  }
 }
