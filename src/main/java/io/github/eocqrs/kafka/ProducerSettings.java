4702b4433fdf9fe0b2ef5ff2bf1509dd4d80ef13
h1alexbel <aliaksei.bialiauski@hey.com>
2024-05-30 14:08:18 +0300

    feat(#507): license years

--- src/main/java/io/github/eocqrs/kafka/ProducerSettings.java
+++ src/main/java/io/github/eocqrs/kafka/ProducerSettings.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2022 Aliaksei Bialiauski, EO-CQRS
+ *  Copyright (c) 2023-2024 Aliaksei Bialiauski, EO-CQRS
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
